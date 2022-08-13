9b6f3c0137eca5b21920ff93179084e4e98f102b
Ori Maci <djorion317@gmail.com>
2022-08-13 16:07:49 -0400

    async thunk with error state message

--- src/features/cart/rx/Cart.slice.ts
+++ src/features/cart/rx/Cart.slice.ts
@@ -10,11 +10,13 @@ export interface CartState {
     [productID: string]: number;
   },
   checkoutState: CheckoutState;
+  errorMessage: string;
 }
 
 const initialState: CartState = {
   items: {},
-  checkoutState: "READY"
+  checkoutState: "READY",
+  errorMessage: ""
 }
 
 export const checkoutCart = createAsyncThunk("cart/checkout", async (items: CartItems) => {
@@ -44,14 +46,15 @@ export const cartSlice = createSlice({
     }
   },
   extraReducers: function(builder) {
-    builder.addCase(checkoutCart.pending, (state, action) => {
+    builder.addCase(checkoutCart.pending, (state) => {
       state.checkoutState = "LOADING";
     })
-    builder.addCase(checkoutCart.fulfilled, (state, action) => {
+    builder.addCase(checkoutCart.fulfilled, (state) => {
       state.checkoutState = "READY";
     })
     builder.addCase(checkoutCart.rejected, (state, action) => {
       state.checkoutState = "ERROR";
+      state.errorMessage = action.error.message || "";
     })
   }
 });
